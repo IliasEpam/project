0c77943ecd7a06fb2e8adc0b989d129d30a1291e
IliasEpam <fort-92@mail.ru>
2017-04-12 16:53:10 +0400

    init

---
+++ src/scripts/app.js
@@ -0,0 +1,13 @@
+import _ from 'lodash';
+import $ from 'jquery';
+import * as header from './components/header';
+import * as content from './components/content';
+import * as footer from './components/footer';
+import * as menu from './components/menu';
+
+header.init();
+menu.init();
+footer.init();
+content.init();
+
+console.log(_.VERSION, $.fn.jquery);
