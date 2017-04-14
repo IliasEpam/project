15674b6aa14f448c03bd765f0a3f7ef5c73481bb
westfort29 <westfort29@gmail.com>
2017-04-14 11:40:55 +0400

    sass injection(try) + BEM injection(try)

--- src/app.js
+++ src/app.js
@@ -8,48 +8,49 @@ var main = function(){
 		$('.grid-view').parent().addClass('none');
 		$('.list-view').parent().removeClass('none');
 		$('.good').css({"display":"inline-block", "width":"200", "height":"200"});
-		$('.good-wrap').css({"width":"200"});
-		$('.good-desc').css({"display":"block", "width":"100%"});
+		$('.good__wrap').css({"width":"200"});
+		$('.good__desc').css({"display":"block", "width":"100%"});
 		$('.good__price').css({"display":"block", "width":"100%", "text-align":"center" });
 		$('.good__name').css({"display":"block", "width":"100%", "text-align":"center"});
 		$('.good__desc-wide').addClass('none');
-		$('.good-info').css({"display":"block", "height":"auto"});
+		$('.good__info').css({"display":"block", "height":"auto"});
 	});
 	$('.list-view').click(function(){
 		$('.list-view').parent().addClass('none');
 		$('.grid-view').parent().removeClass('none');
 		$('.good').css({"display":"block", "width":"100%", "height":"100%"});
-		$('.good-wrap').css({"width":"600", "text-align":"left"});
-		$('.good-desc').css({"display":"inline-block", "width":"49%"});
-		$('.good__price').css({"display":"inline-block", "width":"49%", "text-align":"left"});
-		$('.good__name').css({"display":"inline-block", "width":"49%", "text-align":"left"});
+		$('.good__wrap').css({"width":"600", "text-align":"left"});
+		$('.good__desc').css({"display":"inline-block", "width":"49%"});
+		$('.good__price').css({"display":"inline-block", "width":"28%", "text-align":"left"});
+		$('.good__name').css({"display":"inline-block", "width":"70%", "text-align":"left"});
 		$('.good__desc-wide').removeClass('none');
-		$('.good-info').css({"display":"flex", "align-content":"space-between", "flex-wrap":"wrap", "height":"160"});
+		$('.good__info').css({"display":"flex", "align-content":"space-between", "flex-wrap":"wrap", "height":"160"});
 	});
 	
 	/*****************registartion and logging in animation**********************/
-	$('.profile').click(function(){
+	$('.navigation-top__profile').click(function(){
 		$('.modal-background').removeClass('none');
-		$('.modal-login').removeClass('none');
+		$('#sign-window').removeClass('none');
 	});
 	$('.modal-background').click(function(){
 		$('.modal-background').addClass('none');
-		$('.modal-login').addClass('none');
-		$('.modal-register').addClass('none');
+		$('#sign-window').addClass('none');
+		$('#reg-window').addClass('none');
 	});
 	$('.reg').click(function(){
-		$('.modal-register').removeClass('none');
-		$('.modal-login').addClass('none');
+		$('#reg-window').removeClass('none');
+		$('#sign-window').addClass('none');
 	});
 	$('.sign').click(function(){
-		$('.modal-register').addClass('none');
-		$('.modal-login').removeClass('none');
+		$('#reg-window').addClass('none');
+		$('#sign-window').removeClass('none');
 	});
-	$('.modal-close').click(function(){
+	$('.modal-window__close').click(function(){
 		$('.modal-background').addClass('none');
-		$('.modal-login').addClass('none');
-		$('.modal-register').addClass('none');		
+		$('#sign-window').addClass('none');
+		$('#reg-window').addClass('none');		
 	});
 	
-}
+};
 $(document).ready(main);
+console.log('script is here');
